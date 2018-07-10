d8004762d35ce2eb9f238e7118c8bd4f8fee8e19
Mihir Gandhi <gandhi.mi@husky.neu.edu>
2018-07-10 17:21:23 -0400

    user-admin web service

--- src/main/java/com/example/webdevsummer22018serverjavamihirgandhi/services/UserService.java
+++ src/main/java/com/example/webdevsummer22018serverjavamihirgandhi/services/UserService.java
@@ -1,10 +1,14 @@
 package com.example.webdevsummer22018serverjavamihirgandhi.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +32,29 @@ public class UserService {
 	public List<User> findAllUsers() {
 		return (List<User>) userRepository.findAll();
 	}
+	
+	@PostMapping("/api/user")
+	public User createUser(@RequestBody User user) {
+		return userRepository.save(user);
+	}
+	
+	
+	@GetMapping("/api/user/{userId}")
+	public Optional<User> findUserById(int userId) {
+		return userRepository.findById(userId);
+	}
+	
+	@PutMapping("/api/user/{userId}")
+	public User updateUser(@PathVariable("userId") int userId, @RequestBody User newUser) {
+		newUser.setId(userId);
+		userRepository.save(newUser);
+		return newUser;
+	}
+	
+	@DeleteMapping("/api/user/{userId}")
+	public void deleteUser(@PathVariable("userId") int id) {
+		userRepository.deleteById(id);
+	}
+
 	
 }

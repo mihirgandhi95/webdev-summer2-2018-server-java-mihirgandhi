62ec3f13c4f0db96e3f945a641ef8408de811724
Mihir Gandhi <gandhi.mi@husky.neu.edu>
2018-07-10 16:58:03 -0400

    completed five functions for user.service.client.js

--- src/main/webapp/jquery/services/user.service.client.js
+++ src/main/webapp/jquery/services/user.service.client.js
@@ -4,11 +4,66 @@ function UserServiceClient() {
     this.findUserById = findUserById;
     this.deleteUser = deleteUser;
     this.updateUser = updateUser;
-    this.url = 'http://localhost:8080/api/user';
+
+
+    this.url = '/api/user';
     var self = this;
-    function createUser(user, callback) { … }
-    function findAllUsers(callback) { … }
-    function findUserById(userId, callback) { … }
-    function updateUser(userId, user, callback) { … }
-    function deleteUser(userId, callback) { … }
+
+
+
+
+    function createUser(user, callback) {
+        return fetch(self.url, {
+            method: 'post',
+            body: JSON.stringify(user),
+            headers: {
+                'content-type': 'application/json'
+            }
+        }).then(callback);
+    }
+
+
+
+
+
+    function findAllUsers(callback) {
+        return fetch(self.url)
+            .then(function (response) {
+                return response.json();
+            });
+    }
+
+
+
+    function findUserById(userId, callback) {
+        return fetch(self.url + '/' + userId)
+            .then(function(response){
+                return response.json();
+            });
+
+    }
+
+
+
+    function updateUser(user, userId, callback) {
+        return fetch(self.url + '/' + userId, {
+            method: 'put',
+            body: JSON.stringify(user),
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+            .then(callback);
+    }
+
+
+
+
+    function deleteUser(userId, callback) {
+        return fetch(self.url + '/' + userId, {
+            method: 'delete'
+        }).then(callback);
+    }
+
+    
 }

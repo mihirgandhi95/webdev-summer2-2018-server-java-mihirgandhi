380c4a62bae8168d6ae65e70a4d4ea4afd724b1d
Mihir Gandhi <gandhi.mi@husky.neu.edu>
2018-07-10 17:04:20 -0400

    worked on user mode for MySql using plain java getter/setter

--- src/main/java/com/example/webdevsummer22018serverjavamihirgandhi/models/User.java
+++ src/main/java/com/example/webdevsummer22018serverjavamihirgandhi/models/User.java
@@ -12,8 +12,38 @@ public class User {
 	private Integer id;
 	private String username;
 	private String password;
+	private String email;
+	private String phone;
+	private String role;
+	private String dateOfBirth;
 	private String firstName;
 	private String lastName;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	
 	public String getUsername() {
 		return username;
 	}

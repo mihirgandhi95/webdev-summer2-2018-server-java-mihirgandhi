419262a46dee060cdb5955cc9b0b3f92e2c6d481
Mihir Gandhi <gandhi.mi@husky.neu.edu>
2018-07-16 00:57:23 -0400

    added course data model

---
+++ src/main/java/com/example/webdevsummer22018serverjavamihirgandhi/repositories/CourseRepository.java
@@ -0,0 +1,11 @@
+package com.example.webdevsummer22018serverjavamihirgandhi.repositories;
+
+
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.webdevsummer22018serverjavamihirgandhi.models.Course;
+
+
+public interface CourseRepository
+extends CrudRepository<Course, Integer> { }
